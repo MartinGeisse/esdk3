6e5903dce93e9312b4679f12c52b324a24880ba6
Martin Geisse <martingeisse@googlemail.com>
2022-03-03 19:37:53 +0100

    ...

--- src/main/java/name/martingeisse/esdk/plot/DesignPlot.java
+++ src/main/java/name/martingeisse/esdk/plot/DesignPlot.java
@@ -6,9 +6,14 @@ import com.google.common.collect.ImmutableList;
  * Plot objects are immutable, but not value objects in general. That is, their identity does matter, and is
  * usually used for referencing.
  *
- * There is no such thing as initial values. Each value plot only consists of updates. For those value plots for which
- * an initial value makes sense at all, it must come with the first update. Before / without that update, default
- * values apply.
+ * The initial values are defined through an initializer event that is treated the same way as other events with
+ * respect to the updated values, but with special treatment in terms of timing. It is defined to happen before all
+ * events, with no reference to a specific point in time, and does not take part in event numbering -- the first
+ * event after the initializer, i.e. the first event in the events list, is event number 0. If affects the values as
+ * follows: Before the initializer, all values are set to a pre-initialized value that depends on the value plot
+ * descriptor, such as false for bit-typed plots and 0 for vector-typed plots. The initializer event is applied as
+ * an update to this pre-initialized state to obtain the initialized state, just like later events are applied as
+ * state updates to obtain the next state.
  *
  * The trigger that causes events and their values to be plotted defines how the timing of these events must be
  * interpreted. The common case is clock-triggered, which means that each event contains the values that are stable
@@ -17,15 +22,14 @@ import com.google.common.collect.ImmutableList;
  * consistent with register values, in the sense of the operators that define the derived signals. Value plots
  * that describe changes to the current state, as opposed to the current state itself, should be defined in a way
  * that is consistent with this definition.
- *
- * TODO define the timing of the initialization events / values
  */
 public final class DesignPlot {
 
     public final ImmutableList<ValuePlotDescriptor> valuePlotDescriptors;
+    public final Event initializer;
     public final ImmutableList<Event> events;
 
-    public DesignPlot(ImmutableList<ValuePlotDescriptor> valuePlotDescriptors, ImmutableList<Event> events) {
+    public DesignPlot(ImmutableList<ValuePlotDescriptor> valuePlotDescriptors, Event initializer, ImmutableList<Event> events) {
         if (valuePlotDescriptors == null) {
             throw new IllegalArgumentException("valuePlotDescriptors is null");
         }
@@ -34,6 +38,10 @@ public final class DesignPlot {
                 throw new IllegalArgumentException("valuePlotDescriptors contains null element");
             }
         }
+        if (initializer == null) {
+            throw new IllegalArgumentException("initializer is null");
+        }
+        initializer.validate(valuePlotDescriptors);
         if (events == null) {
             throw new IllegalArgumentException("events is null");
         }
@@ -41,8 +49,10 @@ public final class DesignPlot {
             if (event == null) {
                 throw new IllegalArgumentException("elements contains null element");
             }
+            event.validate(valuePlotDescriptors);
         }
         this.valuePlotDescriptors = valuePlotDescriptors;
+        this.initializer = initializer;
         this.events = events;
     }
 
